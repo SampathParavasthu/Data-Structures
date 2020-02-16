ee00cbd103d2ac418cd1cb4670f89ee7ca7734c2
Sampath Paravasthu <write2sampath@gmail.com>
2020-02-16 18:53:47 +0530

    Linked list basic setup done

--- linkedList/main.js
+++ linkedList/main.js
@@ -1,5 +1,31 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.tail = null;
+  }
+}
 class LinkedList {
-  constructor() {}
+  constructor() {
+    this.head = null;
+  }
+
+  insert(value) {
+
+  }
+
+  remove(value) {
+
+  }
+
+  search(value) {
+
+  }
+
+  printList() {
+
+  }
+
 }
 
-export default LinkedList;
+
+// export default LinkedList;
